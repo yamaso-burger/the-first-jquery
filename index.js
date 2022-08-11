851b7770368ad81815736df1cf661ae579ad6cc8
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-11 11:22:27 +0900

    add some jQuery query again

--- index.js
+++ index.js
@@ -2,4 +2,29 @@
 $('h1').addClass("big-title margin-50");
 
 // document.querySelectorAll("button") also changes...
-$("button");
+$("button").text("Don't Click Me");
+
+// manipulating attribute
+$("a").attr("href", "https://yahoo.com");
+
+//adding event listener
+$("h1").click(function(){
+    $("h1").css("color", "purple");
+});
+
+$("button").click(function(){
+    $("h1").css("color", "red");
+});
+
+$("input").keypress(function(event){
+    $("h1").text(event.key);
+});
+
+$("h1").mouseover(function(){
+    $("h1").css("color", "black");
+});
+
+// animation with jQuery
+$("button").click(function(){
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+});
